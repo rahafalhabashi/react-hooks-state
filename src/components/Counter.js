0e92e17f33d3d65a3afeb26dae153ce768ab2a54
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-04-07 18:20:24 -0400

    commit tree 0490c3715fed94becc5fa0ec3827109417244083

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,44 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   function increment() {
+//     setCount(count + 1);
+//   }
+
+//   return <button onClick={increment}>I have been clicked {count} times</button>;
+// }
+
+// export default Counter;
+
+
+import React, { useState } from "react" //imported the useState hook from React
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  //creating a component function that will return our JSX and execute functions for it
+ 
+  const [count, setCount] = useState(0) 
+  //calling useState inside component: creating a new internal state for the component with an initial value of 0
+  //useState returns 2 variables: 
+    // 1. count(ref to the current value of state in react's internals)
+    // 2. setCount( a setter function to update that state)
 
   function increment() {
-    setCount(count + 1);
+    console.log(count)
+    setCount((currentCount) => currentCount +1)
+    console.log(count)
+   
+
+    //setCount does 2 things:
+      // 1. updates value of count in React's internal state to be count + 1
+      // 2. automatically re-renders component and all its children and updates DOM based on new values for state!
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return (
+    <button onClick={increment}>I have been clicked {count} times.</button>
+    //{count} displays the current value in the button element
+  )
 }
 
-export default Counter;
+export default Counter
